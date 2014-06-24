389dadea92b4adb8707bae416dfe34a62c737064
Pranav Ranawat <pranavmahendra@pranavs-iMac.local>
2014-06-24 17:42:59 +0530

    added a sample class

---
+++ Git Test/PRSample.h
@@ -0,0 +1,13 @@
+//
+//  PRSample.h
+//  Git Test
+//
+//  Created by Pranav Ranawat on 24/06/14.
+//  Copyright (c) 2014 Pranav Ranawat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PRSample : NSObject
+
+@end
---
+++ Git Test/PRSample.m
@@ -0,0 +1,13 @@
+//
+//  PRSample.m
+//  Git Test
+//
+//  Created by Pranav Ranawat on 24/06/14.
+//  Copyright (c) 2014 Pranav Ranawat. All rights reserved.
+//
+
+#import "PRSample.h"
+
+@implementation PRSample
+
+@end
